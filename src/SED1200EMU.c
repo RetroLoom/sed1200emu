28d4b205bb12328c7d2792791b39eef5e9bf7a57
Daniel Arnold <skadarnold@gmail.com>
2024-11-09 12:51:41 -0700

    Experimental Section Triggers
    None of this is working the way I was expecting, so I am ditching it. However I would like to save my work in a commit in case I ever need to refer back to it for some reason.

--- src/SED1200EMU.c
+++ src/SED1200EMU.c
@@ -13,10 +13,10 @@
 volatile uint8_t SEDBuffer [2][20];
 volatile uint8_t SEDRow = 0, SEDCol = 0;
 
-uint8_t SEC_POS = 0;
-uint8_t SEC_COL = 0;
-uint8_t SEC_OFFSET = 0;
-uint8_t SEC_SHOW = 0;
+int8_t SEC_POS = 0;
+int8_t SEC_SIZE = 0;
+int8_t SEC_OFFSET = 0;
+int8_t SEC_SHOW = 0;
 uint32_t SEC_LAST_UPDATE = 0;
 
 extern volatile uint8_t queueHead, queueTail;
@@ -53,10 +53,10 @@ void updateDisplay()
         LCD_SendCmd(LCD_SET_CURSOR | cursorPos);
         
         // Draw each column in the row
-		uint8_t col;
+		int8_t col;
 		if (SEC_SHOW)
 		{
-			for (col = 0; col < SEC_COL + SEC_OFFSET; col++)
+			for (col = 0; col <= SEC_SIZE; col++)
 			{
 				uint8_t c = SEDBuffer[0][SEC_POS + col];
 				// if (c == 0x00) {
@@ -66,12 +66,51 @@ void updateDisplay()
 			}
 
 			// Clear empty space
-			for (col = 0; col < 20 - SEC_COL + SEC_OFFSET; col++)
-            	LCD_SendChar(ASCII_SPACE);  // Send character to display
+			for (col = 0; col < 20 - SEC_POS; col++)
+				LCD_SendChar(ASCII_SPACE);  // Send character to display
 		}
 		else
 		{
-			for (col = 0; col <= SEC_POS - SEC_OFFSET + 1; col++)
+			for (col = 0; col < SEC_POS; col++)
+			{
+				uint8_t c = SEDBuffer[0][col];
+				// if (c == 0x00) {
+				//	c = ASCII_SPACE;  // Replace NULL bytes with spaces
+				// }
+				LCD_SendChar(c); // Send character to display
+			}
+		}
+	}
+	//
+
+	/* Draw buffer to display - old
+	//uint8_t row;
+    for (row = 1; row < 2; row++)  // Iterate both rows
+    {
+        // Set the cursor to the start of the current row
+        uint8_t cursorPos = (row == 0 ? 0x00 : 0x40);  // DDRAM addresses
+        LCD_SendCmd(LCD_SET_CURSOR | cursorPos);
+        
+        // Draw each column in the row
+		int8_t col;
+		if (SEC_SHOW)
+		{
+			for (col = 0; col <= SEC_SIZE; col++)
+			{
+				uint8_t c = SEDBuffer[0][SEC_POS + col];
+				// if (c == 0x00) {
+				//	c = ASCII_SPACE;  // Replace NULL bytes with spaces
+				// }
+				LCD_SendChar(c); // Send character to display
+			}
+
+			// Clear empty space
+			for (col = 0; col < 20 - SEC_POS; col++)
+				LCD_SendChar(ASCII_SPACE);  // Send character to display
+		}
+		else
+		{
+			for (col = 0; col < SEC_POS; col++)
 			{
 				uint8_t c = SEDBuffer[0][col];
 				// if (c == 0x00) {
@@ -84,6 +123,7 @@ void updateDisplay()
 		
 
 	}
+	*/
 }
 
 int main () 
@@ -102,10 +142,6 @@ int main ()
     LCD_SendChar(CGRAM_ON); LCD_SendChar(CGRAM_ON); LCD_SendChar(CGRAM_ON);
     LCD_SendChar(CGRAM_OFF); LCD_SendChar(CGRAM_ON); LCD_SendChar(CGRAM_ON);
     LCD_SendChar(CGRAM_ON); LCD_SendChar(CGRAM_OFF);
-
-	int page = 1;
-	uint32_t last_update = 0; // Initialize a timestamp variable
-    uint32_t delay_interval = 2000; // Set delay in milliseconds
 
 	while (1)
 	{
@@ -135,29 +171,33 @@ int main ()
 					SEDCol = address - 0x40; // Subtract 0x40 to get the column in Row 1
 				}
 
+				/*
 				if (SEDCol == 17)
 				{
-					SEC_OFFSET = 4;
-					SEC_POS = SEDCol - SEC_OFFSET;
-					SEC_COL = 0;
+					SEC_OFFSET = -4;
+					SEC_POS = SEDCol + SEC_OFFSET;
+					SEC_SIZE = 1 - SEC_OFFSET;
 					SEC_SHOW = 1;
 					SEC_LAST_UPDATE = millis;
 				}
-				//else
-				//{
-				//	SED_SEC_COL = 0;
-				//}
 
-				if (SEDCol == 0)
+				if (SEDCol == 9)
 				{
-					SEC_COL = 0;
-					//SEC_SHOW = 0;
+					SEC_OFFSET = 1;
+					SEC_POS = SEDCol + SEC_OFFSET;
+					SEC_SIZE = 1 - SEC_OFFSET;
+					SEC_SHOW = 1;
+					SEC_LAST_UPDATE = millis;
 				}
+				*/
 
-				page = 1;
-
+				//if (SEDCol > 16)
+				//{
+				//	SEC_SIZE = 0;
+				//}
+				
 				//LCD_GotoXY (1,17);
-				//LCD_SendStr ("%d", SEDCol);
+				//LCD_SendStr ("%d  ", SEDCol);
 			}
 			else
 			{
@@ -165,6 +205,11 @@ int main ()
 				{
 				case 0x02:
 					c = CGRAM1;
+
+					SEC_POS = SEDCol;
+					SEC_SHOW = 1;
+					SEC_SIZE = 20 - SEC_POS;
+					SEC_LAST_UPDATE = millis;
 					break;
 
 				case 0x01:
@@ -180,12 +225,14 @@ int main ()
 					break;
 				}
 
+				if (SEDCol > SEC_POS)
+				{
+					
+				}
+
 				// Send the character to the LCD; if it's '|', send the CGRAM custom char 0 instead
 				//LCD_SendChar((c == 0x7c) ? 0x00 : c); // Convert '|' (0x7c) to CGRAM character 0
-				SEDBuffer[SEDRow][SEDCol] = c;
-
-				if (SEC_POS)
-					SEC_COL++;
+				SEDBuffer[SEDRow][SEDCol] = c;			
 				
 				// Move the cursor within the buffer for the next character
 				if (++SEDCol >= 20)
